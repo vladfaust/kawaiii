aa6c74a74f77a525d150df3fc97f9396ce615d65
Vlad Faust <hey@vladfaust.com>
2023-03-27 19:36:16 +0530

    fix(api): error logging

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,15 @@
 import * as server from "./server";
 import * as chainSync from "./chainSync";
 import Sentry from "./services/sentry";
+import config from "./config";
 
-process.on("uncaughtException", function (e) {
-  Sentry.captureException(e);
-  process.exit(1);
-});
+if (config.sentry) {
+  process.on("uncaughtException", function (e) {
+    Sentry.captureException(e);
+    console.error(e);
+    process.exit(1);
+  });
+}
 
 server.listen();
 chainSync.sync();
