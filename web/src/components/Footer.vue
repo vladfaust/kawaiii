4dba8c453a96243e2f9bc13e8de0b831ad979b7f
Vlad Faust <hey@vladfaust.com>
2023-03-27 12:50:11 +0530

    fixup! ux: help page

--- web/src/components/Footer.vue
+++ web/src/components/Footer.vue
@@ -5,4 +5,7 @@ footer.flex.h-16.w-full.justify-center.border-y.p-4
   .flex.w-full.max-w-3xl.items-center.justify-between
     ul.flex.gap-3
       li kawaiii.co © 2023
+    ul.flex.gap-3
+      li
+        RouterLink.link-hover(to="/help") Help
 </template>
